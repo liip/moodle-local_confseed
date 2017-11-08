9f432588c1acd5865c9bbe0037636a8b84f88f24
Didier 'OdyX' Raboud <didier.raboud@liip.ch>
2017-11-08 12:54:07 +0100

    Reorder categories and fields only once

--- db/upgrade.php
+++ db/upgrade.php
@@ -41,7 +41,6 @@ function xmldb_local_confseed_upgrade($oldversion) {
 
     // Create or update user profile fields.
     if (isset($CFG->CONFSEED->user_info_fields)) {
-        require_once($CFG->dirroot . '/user/profile/definelib.php');
         foreach ($CFG->CONFSEED->user_info_fields as $newfield) {
             if (!isset($newfield->shortname) ||
                 !isset($newfield->name) ||
@@ -57,9 +56,10 @@ function xmldb_local_confseed_upgrade($oldversion) {
             } else {
                 $DB->insert_record('user_info_field', $newfield);
             }
-            profile_reorder_fields();
-            profile_reorder_categories();
         }
+        require_once($CFG->dirroot . '/user/profile/definelib.php');
+        profile_reorder_fields();
+        profile_reorder_categories();
     }
 
     // An upgrade_plugin_savepoint call is not needed here as upgradelib.php's upgrade_plugins() will do it for us.
