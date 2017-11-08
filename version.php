dafe8c20bb92e90078d9ab2be2555c7964508e3f
Didier 'OdyX' Raboud <didier.raboud@liip.ch>
2017-11-08 09:59:19 +0100

    Initial version file, with the possibility to force an upgrade run

---
+++ version.php
@@ -0,0 +1,31 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Configuration Seeder version
+ *
+ * @package local_confseed
+ * @author Liip <https://www.liip.ch/>
+ * @author Didier Raboud <didier.raboud@liip.ch>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version   = (isset($CFG->CONFSEED) && isset($CFG->CONFSEED->version)) ? $CFG->CONFSEED->version : '2017110700';
+$plugin->requires  = 2017051502; // Requires Moodle 3.3.
+$plugin->component = 'local_confseed';
+
